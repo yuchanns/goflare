99e8499917fa4d49991468210b187c250ece61cc
Hanchin Hsieh <me@yuchanns.xyz>
2024-11-15 10:22:43 +0800

    Display README in package detail

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,34 @@
 import { Hono } from 'hono'
-import { html } from 'hono/html'
+import { html, raw } from 'hono/html'
 import { HTTPException } from 'hono/http-exception'
+import { marked } from 'marked'
+
+const mdrender = new marked.Renderer()
+
+mdrender.blockquote = ({ tokens }) => {
+  const contents = []
+  for (const token of tokens) {
+    if (token.type == "paragraph") {
+      contents.push(`<p><em>${token.raw}</em></p>`)
+    } else {
+      contents.push(marked.parseInline(token.raw))
+    }
+  }
+  return `<blockquote>${contents.join()}</blockquote>`
+}
+
+mdrender.code = ({ text, lang }) => {
+  if (!lang || lang == "") {
+    lang = "plaintext"
+  }
+  return `<div class="code ${lang}"><pre><code>${text}</code></pre></div>`
+}
 
 type Bindings = {
   PATHS: {
     pkg: string
     repo: string
+    readme: string
   }[]
   AVATAR: string
   DESC: string
@@ -63,7 +86,7 @@ ${c.env.PATHS.map(({ pkg: path }) => {
 </html>`)
 })
 
-app.get('/:pkg/*', (c) => {
+app.get('/:pkg/*', async (c) => {
   const pkg = c.req.param("pkg")
   const pc = c.env.PATHS.find(({ pkg: path }) => path == pkg)
   if (!pc) {
@@ -73,7 +96,14 @@ app.get('/:pkg/*', (c) => {
   const content_import = `${u.hostname}/${pc.pkg} git ${pc.repo}`
   const content_source = `${u.hostname}/${pc.pkg} ${pc.repo} ${pc.repo}/tree/main{/dir} ${pc.repo}/blob/main{/dir}/{file}#L{line}`
   const pkg_url = `https://pkg.go.dev/${u.hostname}/${u.pathname.slice(1)}`
-  const content_refresh = `0; url=${pkg_url}`
+  let readme = ""
+  try {
+    if (pc.readme != "") {
+      readme = await (await fetch(pc.readme)).text()
+    }
+  } catch (e) {
+    console.error(e)
+  }
   return c.html(html`<!DOCTYPE html>
 <html>
 <head>
@@ -82,10 +112,43 @@ app.get('/:pkg/*', (c) => {
   <meta name="go-import" content="${content_import}">
   <meta name="go-source" content="${content_source}">
   <title>${pc.pkg}</title>
+<style>
+body {
+  font-family: 'Cantarell,sans-serif';
+  font-size: 120%;
+  max-width: 40em;
+  margin: 0 auto;
+  padding: 0 1em 1em 1em;
+  line-height: 1.6;
+}
+
+header {
+  text-align: center;
+}
+
+a{
+  text-decoration: underline;
+  font-weight: bold;
+  cursor: pointer;
+  color: inherit;
+}
+
+a:visited {
+  color: inherit;
+}
+</style>
 </head>
-<body>
-<a href="${pkg_url}">Redirecting to documentation...</a>
-</body>`)
+<header><h1> ${u.hostname}</h1></header>
+<p>
+<img style="float: right; width: 9em; margin-left: 1em; border-radius: 15px;" src="${c.env.AVATAR}" />
+${c.env.DESC}
+</p>
+<hr>
+<div>
+${raw(await marked.parse(readme, { renderer: mdrender }))}
+</div>
+<a href="${pkg_url}">Head to documentation.</a>
+</html>`)
 })
 
 export default app
