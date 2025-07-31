3e9987d4221f8b369cc7b48c0a36fdf4908a965a
Hanchin Hsieh <me@yuchanns.xyz>
2025-07-31 10:25:35 +0800

    style: use hljs

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,17 @@ import { Hono } from 'hono'
 import { html, raw } from 'hono/html'
 import { HTTPException } from 'hono/http-exception'
 import { marked } from 'marked'
+import { markedHighlight } from 'marked-highlight'
+import hljs from 'highlight.js'
+
+marked.use(markedHighlight({
+  emptyLangClass: 'hljs',
+  langPrefix: 'hljs language-',
+  highlight(code, lang, info) {
+    const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+    return hljs.highlight(code, { language }).value;
+  }
+}))
 
 const mdrender = new marked.Renderer()
 
@@ -21,7 +32,7 @@ mdrender.code = ({ text, lang }) => {
   if (!lang || lang == "") {
     lang = "plaintext"
   }
-  return `<div class="code ${lang}"><pre><code>${text}</code></pre></div>`
+  return `<div class="highlight"><pre><code class="language-${lang}">${text}</code></pre></div>`
 }
 
 type Bindings = {
@@ -45,28 +56,56 @@ app.get('/', (c) => {
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 <title>${u.hostname} Go Packages</title>
 <style>
+:root {
+    --border-bottom-color: #737373;
+    --font-family: Cantarell,sans-serif;
+}
+
 body {
-  font-family: 'Cantarell,sans-serif';
-  font-size: 120%;
-  max-width: 40em;
-  margin: 0 auto;
-  padding: 0 1em 1em 1em;
-  line-height: 1.6;
+    font-family: var(--font-family);
+    font-size: 120%;
+    max-width: 40em;
+    margin: 0 auto;
+    padding: 0 1em 1em 1em;
+    line-height: 1.6;
 }
 
 header {
-  text-align: center;
+    text-align: center;
 }
 
-a{
-  text-decoration: underline;
-  font-weight: bold;
-  cursor: pointer;
-  color: inherit;
+a.action {
+    cursor: pointer;
+    font-style: italic;
+    font-size: 80%;
 }
 
-a:visited {
-  color: inherit;
+a.title {
+    text-decoration: underline;
+    font-weight: bold;
+    cursor: pointer;
+}
+
+a.button {
+    text-decoration: none;
+    cursor: pointer;
+}
+
+a.title {
+    text-decoration: none;
+    color: #000000;
+}
+
+a.title:visited {
+    color: #000000;
+}
+
+a:not(.button, .action, .title):visited {
+    color: #902000;
+}
+
+a:not(.button, .action, .title) {
+    color: #902000;
 }
 </style>
 </head>
@@ -112,28 +151,356 @@ app.get('/:pkg/*', async (c) => {
   <meta name="go-source" content="${content_source}">
   <title>${pc.pkg}</title>
 <style>
+:root {
+    --border-bottom-color: #737373;
+    --font-family: Cantarell,sans-serif;
+}
+
 body {
-  font-family: 'Cantarell,sans-serif';
-  font-size: 120%;
-  max-width: 40em;
-  margin: 0 auto;
-  padding: 0 1em 1em 1em;
-  line-height: 1.6;
+    font-family: var(--font-family);
+    font-size: 120%;
+    max-width: 40em;
+    margin: 0 auto;
+    padding: 0 1em 1em 1em;
+    line-height: 1.6;
 }
 
 header {
+    text-align: center;
+}
+
+a.action {
+    cursor: pointer;
+    font-style: italic;
+    font-size: 80%;
+}
+
+a.title {
+    text-decoration: underline;
+    font-weight: bold;
+    cursor: pointer;
+}
+
+a.button {
+    text-decoration: none;
+    cursor: pointer;
+}
+
+a.title {
+    text-decoration: none;
+    color: #000000;
+}
+
+a.title:visited {
+    color: #000000;
+}
+
+a:not(.button, .action, .title):visited {
+    color: #902000;
+}
+
+a:not(.button, .action, .title) {
+    color: #902000;
+}
+table {
+    margin: auto;
+}
+
+th, td {
+    padding: 8px;
+    text-align: left;
+}
+
+input[type=submit] {
+    text-decoration: none;
+    cursor: pointer;
+    border: none;
+    background: transparent;
+    font-family: var(--font-family);
+    font-size: 100%;
+}
+
+input[type=text] {
+    border: none;
+    border-bottom: 1px inset var(--border-bottom-color);
+    outline: none;
+    font-family: var(--font-family);
+    font-size: 120%;
+}
+
+input[type=password] {
+    border: none;
+    border-bottom: 1px inset var(--border-bottom-color);
+    outline: none;
+    font-family: var(--font-family);
+    font-size: 120%;
+}
+
+.highlight {
+    padding-left: calc(10% - 2em);
+    font-size: 100%;
+    line-height: 1.25;
+}
+
+ul {
+    list-style-type: disc;
+    margin-top: 1.25em;
+    margin-top: 1.25em;
+    padding-left: 1.625em;
+}
+
+ul > li::marker {
+    color: var(--border-bottom-color);
+}
+
+pre {
+    margin: 0;
+}
+
+pre > code {
+    white-space: pre-wrap;
+    word-wrap: break-word;
+    word-break: break-word;
+    overflow-wrap: break-word;
+    position: relative;
+    font-family: monospace;
+    line-height: 1.25;
+}
+
+code:not([class^="language-"])::before {
+    content: "\`";
+}
+
+code:not([class^="language-"])::after {
+    content: "\`";
+}
+
+code:not([class^="language-"]) {
+    font-weight: 600;
+    font-family: var(--font-family);
+}
+
+.radio-group {
+  max-width: 650px;
+  text-align: center;
+  font-family: var(--font-family);
+}
+
+.radio-label {
+  display: inline-block;
+  cursor: pointer;
+}
+
+.radio-text {
+  font-size: 80%;
+  color: var(--border-bottom-color);
+}
+
+.summary > h2 {
+  font-size: 1em;
+  font-weight: normal;
+}
+
+figure > figcaption {
+  font-size: 80%;
+  color: var(--border-bottom-color);
   text-align: center;
 }
 
-a{
-  text-decoration: underline;
-  font-weight: bold;
-  cursor: pointer;
-  color: inherit;
+/* Background */
+.hljs { 
+    background-color: #f0f0f0; 
+    color: inherit;
 }
 
-a:visited {
-  color: inherit;
+/* Line highlighting */
+.hljs-line { display: flex; }
+.hljs-line.highlighted { background-color: #d8d8d8; }
+
+/* Line numbers */
+.hljs-ln-numbers {
+    white-space: pre;
+    -webkit-user-select: none;
+    user-select: none;
+    margin-right: 0.4em;
+    padding: 0 0.4em 0 0.4em;
+    color: #7f7f7f;
+    vertical-align: top;
+    border: 0;
+}
+
+.hljs-ln-code {
+    vertical-align: top;
+    padding: 0;
+    margin: 0;
+    border: 0;
+}
+
+/* Keywords */
+.hljs-keyword,
+.hljs-built_in,
+.hljs-selector-tag,
+.hljs-section,
+.hljs-name {
+    color: #007020;
+    font-weight: bold;
+}
+
+.hljs-keyword.hljs-pseudo {
+    color: #007020;
+    font-weight: normal;
+}
+
+.hljs-type {
+    color: #902000;
+}
+
+/* Names and identifiers */
+.hljs-title,
+.hljs-class .hljs-title {
+    color: #0e84b5;
+    font-weight: bold;
+}
+
+.hljs-function .hljs-title {
+    color: inherit;
+}
+
+.hljs-attr,
+.hljs-attribute {
+    color: #0e84b5;
+    font-weight: bold;
+}
+
+.hljs-variable {
+    color: inherit;
+}
+
+.hljs-tag {
+    color: #062873;
+    font-weight: bold;
+}
+
+.hljs-meta {
+    color: #555;
+    font-weight: bold;
+}
+
+.hljs-doctag {
+    color: #d55537;
+    font-weight: bold;
+}
+
+/* Literals and strings */
+.hljs-string {
+    color: inherit;
+}
+
+.hljs-string.hljs-doc {
+    font-style: italic;
+}
+
+.hljs-string.hljs-escape {
+    font-weight: bold;
+}
+
+.hljs-template-variable,
+.hljs-subst {
+    color: #70a0d0;
+}
+
+.hljs-regexp {
+    color: #235388;
+}
+
+.hljs-symbol {
+    color: #517918;
+}
+
+/* Numbers */
+.hljs-number,
+.hljs-literal {
+    color: #40a070;
+}
+
+/* Operators */
+.hljs-operator {
+    color: #666;
+}
+
+.hljs-operator.hljs-word {
+    color: #007020;
+    font-weight: bold;
+}
+
+/* Comments */
+.hljs-comment,
+.hljs-quote {
+    color: #60a0b0;
+    font-style: italic;
+}
+
+.hljs-comment.hljs-special {
+    color: #60a0b0;
+    background-color: #fff0f0;
+}
+
+.hljs-meta,
+.hljs-meta-keyword {
+    color: #007020;
+}
+
+/* Diff and version control */
+.hljs-deletion {
+    color: #a00000;
+}
+
+.hljs-addition {
+    color: #00a000;
+}
+
+.hljs-emphasis {
+    font-style: italic;
+}
+
+.hljs-strong {
+    font-weight: bold;
+}
+
+/* Generic styles */
+.hljs-error {
+    color: #f00;
+}
+
+.hljs-title.hljs-heading {
+    color: #000080;
+    font-weight: bold;
+}
+
+.hljs-title.hljs-subheading {
+    color: #800080;
+    font-weight: bold;
+}
+
+.hljs-link {
+    text-decoration: underline;
+}
+
+/* Additional highlight.js specific classes */
+.hljs-params {
+    color: inherit;
+}
+
+.hljs-bullet {
+    color: inherit;
+}
+
+.hljs-code {
+    color: inherit;
+}
+
+.hljs-formula {
+    color: inherit;
 }
 </style>
 </head>
